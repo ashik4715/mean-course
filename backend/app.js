5ea1f6420f9fdcc6b4f0d2646e4e7877c87bcbd9
jhalakMazegeek <jhalak.mazegeek@gmail.com>
2020-12-20 01:06:03 +0100

    Server side rendering post without database Mongo

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,44 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
 app.use((req, res, next) => {
-  console.log('First Middleware');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Max-Age", "1800");
+  // res.setHeader("Access-Control-Allow-Headers", "content-type, X-Auth-Token,");
+  // res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express JS');
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully!'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: 'fadf124211',
+      title: 'First Server-Side Post',
+      content: 'This is coming from the server'
+    },
+    { id: 'ksajflaj132',
+      title: 'Second Server-Side Post',
+      content: 'This is coming from the server!'
+    },
+  ];
+  res.status(200).json({
+    message: 'Post fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,7 +33,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
